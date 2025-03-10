ccd22aa5fa5e6ed8ffe1e0c852fdcd406300a908
Nabz863 <2671907@students.wits.ac.za>
2025-03-10 16:30:54 +0200

    Error in Script.js

--- script.js
+++ script.js
@@ -1,1 +1,66 @@
+document.addEventListener("DOMContentLoaded", function() {
+const btnSubmit = document.getElementById("btn");
+const txtInput = document.getElementById("txt");
+const secInfo = document.getElementById("country-info");
+const secBorder = document.getElementById("bordering-countries");
 
+btnSubmit.addEventListener("click", async function() {
+    const strName = txtInput.value.trim();
+    if (!strName) {
+        displayError("Please enter a country name.");
+        return;
+    }
+    try {
+        const datCntryData = await fetchCntryData(strName);
+        if (!datCntryData) {
+            displayError("Country not found.");
+            return;
+        }
+        const neighbours = await fetchNbrData(datCntryData.borders || []);
+        updateDOM(datCntryData, neighbours);
+    } catch(error) {
+        displayError("Error");
+        console.error(error);
+    }
+});
+
+async function fetchCntryData(country) {
+    const response = await fetch("https://restcountries.com/v3.1/name/${country}?fullText=true");
+    if (!response.ok) throw new Error("Country not found.");
+    const datData = await response.json();
+    return data[0];
+}
+
+function updateDOM(countryData, neighbours) {
+    secInfo.innerHTML = `
+                        <ul>
+                         <li>Capital: ${countryData.capital ? countryData.capital[0] : "N/A"}</li>
+                         <li>Population: ${countryData.population.toLocaleString()}</li>
+                         <li>Region: ${countryData.region}</li>
+                         <li>Flag:<br>
+                         <img src="${countryData.flags.svg} alt="${countryData.name.common}" width="150"></li>
+                        </ul>
+                         `;
+                         if (neighbours.length === 0) {
+                            secBorder.innerHTML = `<p>No bordering countries.<\p>`;
+                            return;
+                         }
+    secBorder.innerHTML = `
+                        <ul>
+                            <li>Bordering Countries:
+                            ${neighbours.map(neighbour=> `
+                                <ul>
+                                    <li>${neighbor.name.common}<br>
+                                    <img src="${neighbour.flags.svg}" alt="Flag of ${neighbor.name.common}" width="50"</li>
+                                </ul>
+                                `).join("")}
+                        </ul>
+                        `;  
+}
+
+function displayError(message) {
+    secInfo.innerHTML = `<p class="error">${message}</p>`;
+    secBorder.innerHTML = "";
+}
+
+});
